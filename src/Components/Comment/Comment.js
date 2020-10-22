63ba9d5931229231cf5327cfc2cde55616d1a5b4
ARASHFAQUE <55973326+ARASHFAQUE@users.noreply.github.com>
2020-10-23 02:02:40 +0600

    Second commit

--- src/Components/Comment/Comment.js
+++ src/Components/Comment/Comment.js
@@ -4,21 +4,32 @@ import Details from '../Details/Details';
 
 const Comment = () => {
     const { postId } = useParams();
-    const [ post, setPost ] = useState([]);
+    const [ comment, setComment ] = useState([]);
     useEffect( () => {
         const url = `https://jsonplaceholder.typicode.com/comments?postId=${postId}`;
         fetch(url)
         .then( res => res.json())
         .then( data => {
-            setPost(data)
+            setComment(data)
             // data.map( pt => setPost(pt))
         })
     },[])
-    console.log(post);
+    const [ post, setPost ] = useState({});
+    useEffect( () => {
+      const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+      fetch(url)
+      .then( res => res.json())
+      .then( data => setPost(data))
+    }, [])
+    //console.log(post);
+    //console.log(post);
     return (
         <div>
+            <h1>Title: {post.title}</h1>
+            <h1 style={{fontSize:'20px'}}>Post Body: {post.body}</h1>
+            <h3>Comments</h3>
             {
-                post.map( pt => <Details post={pt}></Details>)
+                comment.map( cmt => <Details comment={cmt}></Details>)
             }
         </div>
     );
--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 
 const Details = ( props ) => {
-    const { name, body } = props.post;
+    const { name, body } = props.comment;
     const commentStyle = {
         background: '#d3d3d3',
-        margin: '10px',
+        marginTop: '10px',
         padding: '10px',
         borderRadius: '10px',
         fontSize: '10px',
     }
     //console.log(props.post.name);
     return (
-        <div style={commentStyle}>
-            <h2>Name: {name.toUpperCase()}</h2>
-            <h4>Body: {body}</h4>
-        </div>
+            <div style={commentStyle}>
+                <h2>Name: {name.toUpperCase()}</h2>
+                <h4>Body: {body}</h4>
+            </div>
     );
 };
 
